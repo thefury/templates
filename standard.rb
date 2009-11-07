b2c94417fd3aef0c85a464992ae8e503d239ae7e
Trevor Oke <trevoroke@trevor-okes-mac-mini.local>
2009-11-06 20:13:21 -0500

    added default views for site

--- standard.rb
+++ standard.rb
@@ -147,6 +147,10 @@ generate(:controller, "site")
 # copy over my own goodies
 remote_file("app/controllers/site_controller.rb", "site_controller.rb")
 remote_file("app/views/layouts/site.html.haml", "layout_site.html.haml")
+run 'echo "You can find me in app/views/site/index.html.haml" > app/views/site/index.html.haml'
+run 'echo "You can find me in app/views/site/privacy.html.haml" > app/views/site/privacy.html.haml'
+run 'echo "You can find me in app/views/site/terms.html.haml" > app/views/site/terms.html.haml'
+
 
 route('map.root :controller => "site", :action => "index"')
 route('map.privacy "/privacy", :controller => "site", :action => "privacy"')
