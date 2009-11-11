d4b9ca36d65c9db45e9722608a3422be1fc05464
Trevor Oke <trevoroke@trevor-okes-mac-mini.local>
2009-11-11 13:06:38 -0500

    fixed path

--- standard/standard.rb
+++ standard/standard.rb
@@ -1,4 +1,4 @@
-REPOSITORYROOT="http://github.com/thefury/templates/raw/master/standard"
+REPOSITORYROOT="http://github.com/thefury/templates/raw/master/standard/"
 
 def commit(message)
   git :add => '.'
